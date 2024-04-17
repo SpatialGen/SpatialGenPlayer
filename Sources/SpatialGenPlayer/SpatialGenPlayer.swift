f4cd83a45e8f7a5efce9e848b58773cc8eebc5a9
Zachary Handshoe <zacharyhandshoe@gmail.com>
2024-04-17 15:41:39 -0500

    init

---
+++ Sources/SpatialGenPlayer/SpatialGenPlayer.swift
@@ -0,0 +1,20 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public class SpatialGenPlayer {
+    
+    public init(){}
+    
+    
+    public func displayVideo(){
+        print("Displaying video")
+    }
+}
+
+
+
+
+
+
